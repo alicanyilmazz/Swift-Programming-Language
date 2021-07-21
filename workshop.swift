f8747178db04f40964f2448d9278b9e06531a877
Alican Yilmaz <49749125+alicanyilmazz@users.noreply.github.com>
2021-07-21 16:25:14 +0300

    Update workshop.swift

--- workshop.swift
+++ workshop.swift
@@ -1,7 +1,11 @@
-// ------------------------------------------- quiz ------------------------------------------------------
 
+
+// ------------------------------------------- Swift Workshop 1 ------------------------------------------------------
+
+// 1.) Verilen sayının basamaklarını birler basamagından baslayarak ayırıp ekrana bastıran program.
 var number : Int = 156
 var arrayOfNumber : [Int] = []
+var reversedArrayOfNumber: [Int] = []
 var counter : Int = 0
 
 while counter<3 {
@@ -10,10 +14,7 @@ while counter<3 {
     counter+=1
 }
 
-var arr : [Int] = []
-var arrayOfNumber2: [Int] = []
-arrayOfNumber2 += arrayOfNumber.reversed()
-for num in arrayOfNumber2 {
+reversedArrayOfNumber += arrayOfNumber.reversed()
+for num in reversedArrayOfNumber {
     print(num)
 }
-
