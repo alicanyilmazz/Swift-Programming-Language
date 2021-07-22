11d35d9b971a3bd954e2857ff6829d83a8a2ade2
Alican Yilmaz <49749125+alicanyilmazz@users.noreply.github.com>
2021-07-22 13:17:33 +0300

    Create differencesotherlang.cs

---
+++ differencesotherlang.cs
@@ -0,0 +1,46 @@
+namespace ProgrammingLanguageSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        public static void mainSample()
+        {
+            string sentence = "alican";
+            string reversedSentence = "";
+            string[] arr = new string[10];
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                reversedSentence += sentence[sentence.Length - 1 - i];
+                // reversedSentence[i] = sentence[sentence.Length - 1 - i]; --> c#(read only okuyabilirsin ama yazamazsın) ve dart dilinde hata verir.ikisindede kod yazım mantıgı aynı.
+                // Cunku String sınıfı immutable ' dır.
+
+            }
+            Console.WriteLine(reversedSentence);
+        }
+
+        public static void secondSample()
+        {
+            var sentence = "alican";
+            var stringBuilder = new StringBuilder(sentence);
+            stringBuilder.Remove(3,2);
+            stringBuilder.Insert(3, "oo");
+            sentence = stringBuilder.ToString();
+            Console.WriteLine(sentence);
+        }
+
+        public static void thirdSample()
+        {
+            var sentence = "alican";
+            sentence=sentence.Replace("i", "x");
+            Console.WriteLine(sentence);
+        }
+    }
+}
+
+
+// Detaylı acıklama için lütfen aşağıdaki siteyi ziyaret edin.
+// https://www.arclab.com/en/kb/csharp/replace-or-remove-char-in-string-by-index.html 
