8cdac5e2d9167792eb82d397efab840588c71e63
Alican Yilmaz <49749125+alicanyilmazz@users.noreply.github.com>
2021-09-02 14:18:42 +0300

    Update swifttechnicalterms.swift

--- swifttechnicalterms.swift
+++ swifttechnicalterms.swift
@@ -1,1 +1,11 @@
+// Swift Technical Terms
 
+// 1.) convenience init()
+// 2.) swift as and is operators
+// 3.) @objc reserved keyword
+// 4.) Retain Cycle, Automatic Reference Counting, Memory Leak 
+// 5.) weak reserved keyword
+// 6.) inout reserved keyword
+// 7.) mutating reserved keyword
+// 8.) label using with parameter
+// 9.) 
