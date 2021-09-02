c22127aa1e6c6047031189c8f84cc515bcb00947
Alican Yilmaz <49749125+alicanyilmazz@users.noreply.github.com>
2021-09-02 14:23:00 +0300

    Update swifttechnicalterms.swift

--- swifttechnicalterms.swift
+++ swifttechnicalterms.swift
@@ -1,11 +1,15 @@
 // Swift Technical Terms
 
 // 1.) convenience init()
-// 2.) swift as and is operators
+// 2.) swift as and is operators (upcasting ve downcasting)
 // 3.) @objc reserved keyword
 // 4.) Retain Cycle, Automatic Reference Counting, Memory Leak 
 // 5.) weak reserved keyword
 // 6.) inout reserved keyword
 // 7.) mutating reserved keyword
 // 8.) label using with parameter
-// 9.) 
+// 9.) Property Observer --> willSet and didSet
+// 10.) Computed Property
+// 11.) Protection Level --> c# daki Access Modifiers
+// 12.) static , final ve class Kavramları
+// 13.) deinit yapısı
